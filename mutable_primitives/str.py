cf8651f2dbeea6c4a55e8cb74bd23d2c3be71a69
demlenar <demlenar@users.noreply.github.com>
2022-11-06 11:45:39 -0500

    add str file to mutable_primitives

---
+++ mutable_primitives/str.py
@@ -0,0 +1,10 @@
+''' Str - A mutable str class '''
+
+from mutable_primitives.base import Mutable
+
+class Str(Mutable):
+    ''' Str - A mutable str class '''
+    base = str
+
+    def __init__(self,val):
+        
